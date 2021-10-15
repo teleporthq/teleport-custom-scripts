dcac5764f15094507c9e44d0b03b67ec274d565c
Vlad <vlad.grecu@teleporthq.io>
2021-10-15 15:38:16 +0300

    add mobile-menu for test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,97 @@
-console.log("Hello World");
+(function () {
+  class Menu {
+    init = () => {
+      this.updateOnRouteChange()
+      this.getMenuElementsAndAddEvents()
+      return this;
+    };
+
+    getMenuElementsAndAddEvents = () => {
+      const menuElements = teleport.getAllElByClass(
+        "teleport-menu-burger"
+      );
+
+      if (!menuElements.length) {
+        teleport.log("No teleport-menu-burger items found");
+        return;
+      }
+
+      menuElements.forEach((burgerMenuElement) => {
+        const mobileMenuElement =
+          burgerMenuElement.nextElementSibling?.className.includes(
+            "teleport-menu-mobile"
+          )
+            ? (burgerMenuElement.nextElementSibling)
+            : null;
+        if (!mobileMenuElement) {
+          teleport.error(
+            `${burgerMenuElement} has no corresponding element with class 'teleport-menu-mobile' as the next sibling.`
+          );
+          return;
+        }
+
+        const closeMenuElement = mobileMenuElement.querySelector(
+          ".teleport-menu-close"
+        );
+        if (!closeMenuElement) {
+          teleport.error(
+            `${mobileMenuElement} has no child element with class 'teleport-menu-close'`
+          );
+          return;
+        }
+
+        burgerMenuElement.addEventListener("click", () => {
+          mobileMenuElement.classList.add("teleport-show");
+        });
+        closeMenuElement.addEventListener("click", () => {
+          mobileMenuElement.classList.remove("teleport-show");
+        });
+      });
+    };
+
+    updateOnRouteChange = () => {
+      let url = location.href;
+      document.body.addEventListener('click', () => {
+        requestAnimationFrame(() => {
+          if (url !== location.href) {
+            console.log('url changed');
+            this.getMenuElementsAndAddEvents()
+            url = location.href
+          }
+        });
+      }, true);
+    }
+  }
+
+  const teleport = {
+    debug: false,
+    log: (msg, obj) => {
+      if (teleport.debug) {
+        console.log("teleport: " + msg, obj || "");
+      }
+    },
+    error: (msg, object) => {
+      console.error("teleport-error: " + msg, object);
+    },
+    getElByClass: (className) => {
+      const el = document.querySelector(`*[class*="${className}"]`);
+
+      if (!el) {
+        teleport.error(`did not find ${className} class`);
+      }
+      return el;
+    },
+    getAllElByClass: (className) => {
+      const elements = document.querySelectorAll(`*[class*="${className}"]`);
+      if (!elements) {
+        teleport.error(`did not find ${className} class`);
+      }
+      return elements;
+    },
+    Menu
+  }
+
+  new Menu().init()
+})()
+
+
