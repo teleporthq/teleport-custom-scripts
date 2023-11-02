46a99a54ee2796ab4972ce6402a4072b9dfee367
Jaya Krishna <namburu1995@gmail.com>
2023-11-02 17:55:03 +0530

    import only slider

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { Dropdown } from './dropdown'
-import { Menu } from './menu'
-import { Accordion } from './accordion'
 import { Slider } from './slider'
 
 let url = location.href;
@@ -9,9 +6,6 @@ document.body.addEventListener(
   () => {
     requestAnimationFrame(() => {
       if (url !== location.href) {
-        new Menu().init();
-        new Accordion().init();
-        new Dropdown().init()
         new Slider().init()
         url = location.href;
       }
@@ -23,16 +17,10 @@ document.body.addEventListener(
 const appDiv = document.getElementById("app");
 if (appDiv) {
   const observer = new MutationObserver(() => {
-    new Dropdown().init()
-    new Menu().init();
-    new Accordion().init();
     new Slider().init()
     observer.disconnect();
   });
   observer.observe(document.body, { childList: true });
 } else {
-  new Dropdown().init()
-  new Menu().init();
-  new Accordion().init();
   new Slider().init()
 }
